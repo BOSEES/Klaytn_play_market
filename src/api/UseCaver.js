28674d19507ada0149ae489a5f0ae845be55725a
c <rladuddlf3@naver.com>
2021-06-06 19:46:18 +0900

    NFT 끝

--- src/api/UseCaver.js
+++ src/api/UseCaver.js
@@ -1,57 +1,69 @@
-import Caver from 'caver-js';
-import CounterABI from '../abi/CounterABI.json';
-import KIP17tokenABI from "../abi/KIP17tokenABI.json";
-import { ACCESS_KEY_ID, SECRET_ACCESS_KEY, COUNT_CONTRACT_ADDRESS, CHAIN_ID, KIP17_CONTRACT_ADDRESS } from '../constants';
+import Caver from "caver-js";
+// import CounterABI from '../abi/CounterABI.json';
+import KIP17ABI from "../abi/KIP17TokenABI.json";
+import {
+  ACCESS_KEY_ID,
+  SECRET_ACCESS_KEY,
+  COUNT_CONTRACT_ADDRESS,
+  NFT_CONTRACT_ADDRESS,
+  CHAIN_ID,
+} from "../constants";
 const option = {
   headers: [
     {
       name: "Authorization",
-      value: "Basic " + Buffer.from(ACCESS_KEY_ID + ":" + SECRET_ACCESS_KEY).toString("base64")
+      value:
+        "Basic " +
+        Buffer.from(ACCESS_KEY_ID + ":" + SECRET_ACCESS_KEY).toString("base64"),
     },
-    {name: "x-chain-id", value: CHAIN_ID}
-  ]
-}
+    { name: "x-chain-id", value: CHAIN_ID },
+  ],
+};
 
-const caver =  new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", option));
-const NftContract = new caver.contract(KIP17tokenABI,KIP17_CONTRACT_ADDRESS);
+const caver = new Caver(
+  new Caver.providers.HttpProvider(
+    "https://node-api.klaytnapi.com/v1/klaytn",
+    option
+  )
+);
+const NFTContract = new caver.contract(KIP17ABI, NFT_CONTRACT_ADDRESS);
 
-export const fetchCardOf = async (address) => {
-  const balance = await NftContract.methods.balanceOf(address).call();
-  console.log(balance);
-
-  const tokenIds = []
-  for (let i = 0; i < balance; i++){
-    const nft = await NftContract.methods.tokenOfOwnerByIndex(address,i).call();
-    tokenIds.push(nft);
+export const fetchCardsOf = async (address) => {
+  // Fetch Balance
+  const balance = await NFTContract.methods.balanceOf(address).call();
+  console.log(`[NFT Balance]${balance}`);
+  // Fetch Token IDs
+  const tokenIds = [];
+  for (let i = 0; i < balance; i++) {
+    const id = await NFTContract.methods.tokenOfOwnerByIndex(address, i).call();
+    tokenIds.push(id);
   }
-  
+  // Fetch Token URIs
   const tokenUris = [];
-  for(let i = 0; i < balance; i++) {
-    const uri = await NftContract.methods.tokenURI(tokenIds[i]).call();
+  for (let i = 0; i < balance; i++) {
+    const uri = await NFTContract.methods.tokenURI(tokenIds[i]).call();
     tokenUris.push(uri);
   }
-
   const nfts = [];
-  for(let i = 0; i < balance; i++) {
-    const nft = {
-      "id": tokenIds[i],
-      "uri": tokenUris[i]
-    }
-    nfts.push(nft);
+  for (let i = 0; i < balance; i++) {
+    nfts.push({ uri: tokenUris[i], id: tokenIds[i] });
   }
-
+  console.log(nfts);
   return nfts;
-}
+};
 
 export const getBalance = (address) => {
   return caver.rpc.klay.getBalance(address).then((response) => {
-    const balance = caver.utils.convertFromPeb(caver.utils.hexToNumberString(response));
+    const balance = caver.utils.convertFromPeb(
+      caver.utils.hexToNumberString(response)
+    );
     console.log(`BALANCE: ${balance}`);
     return balance;
-  })
-}
+  });
+};
 
 // const CountContract = new caver.contract(CounterABI, COUNT_CONTRACT_ADDRESS);
+
 // export const readCount = async () => {
 //   const _count = await CountContract.methods.count().call();
 //   console.log(_count);
@@ -74,5 +86,5 @@ export const getBalance = (address) => {
 //   } catch(e) {
 //     console.log(`[ERROR_SET_COUNT]${e}`);
 //   }
-  
+
 // }
